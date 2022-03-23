a59af29efcf01ed7382d78f2c5905680b873aa82
ZnDeveloper <zndev@ya.ru>
2022-03-24 00:53:53 +0600

    upd

--- src/Commands/config/bundle.php
+++ src/Commands/config/bundle.php
@@ -2,7 +2,9 @@
 
 return [
 //    new \ZnLib\Fixture\Bundle(['container', 'console']),
-    new \ZnLib\Db\Bundle(['container', 'console']),
+//    new \ZnLib\Db\Bundle(['container', 'console']),
+    new \ZnDatabase\Base\Bundle(['container', 'console']),
+    new \ZnDatabase\Tool\Bundle(['container', 'console']),
 //    new \ZnDatabase\Migration\Bundle(['container', 'console']),
 //    new \ZnTool\Package\Bundle(['container', 'console']),
 //    new \ZnTool\Phar\Bundle(['container', 'console']),
