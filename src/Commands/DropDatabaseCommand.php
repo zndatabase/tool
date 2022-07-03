ca4bb7f7c20c1b65a13afb9be9b1e4385c09dddb
ZnDeveloper <zndev@ya.ru>
2022-07-03 14:06:29 +0600

    upd

--- src/Commands/DropDatabaseCommand.php
+++ src/Commands/DropDatabaseCommand.php
@@ -5,7 +5,7 @@ namespace ZnDatabase\Tool\Commands;
 use Symfony\Component\Console\Input\InputOption;
 use ZnDatabase\Base\Console\Traits\OverwriteDatabaseTrait;
 use ZnDatabase\Eloquent\Domain\Capsule\Manager;
-use Illuminate\Support\Collection;
+use ZnCore\Domain\Collection\Libs\Collection;
 use Symfony\Component\Console\Command\Command;
 use ZnCore\Base\Arr\Helpers\ArrayHelper;
 use ZnDatabase\Base\Domain\Facades\DbFacade;
